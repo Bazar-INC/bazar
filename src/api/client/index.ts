7271ce3a2e93c09af7725b328c029564b2d64348
Dima <65868322+byCodeXp@users.noreply.github.com>
2022-06-25 16:59:37 +0300

    update api client

--- src/api/client/index.ts
+++ src/api/client/index.ts
@@ -1,14 +1,48 @@
-import axios from 'axios';
+import axios, { AxiosInstance } from 'axios';
+import { StorageUtility } from "../../utilities/storageUtility";
 
-const BASE_URL = 'https://localhost:7165/api';
+const HOST_NAME = 'https://localhost:7165';
+
+const tokenStorage = new StorageUtility<string>('TOKEN_STORAGE');
+
+const createAxiosInstance = (baseUrl: string) => {
+
+   const client = axios.create({
+      baseURL: `${HOST_NAME}/api/${baseUrl}/`,
+   });
+
+   client.interceptors.request.use(
+      (config) => {
+         const token = tokenStorage.get();
+         if (token) {
+            config.headers = {
+               ...config.headers,
+               Authorization: `Bearer ${token}`
+            };
+         }
+         return config;
+      },
+      (error) => Promise.reject(error)
+   );
+
+   return client;
+}
+
 
 class Client {
-   constructor() {
 
+   private client: AxiosInstance;
+
+   constructor(baseUrl: string) {
+      this.client = createAxiosInstance(baseUrl);
    }
 
-   methodPost(endpoint: string, data?: any) {
-      return axios.post(`${BASE_URL}/${endpoint}`, data);
+   get<TResponse>(url: string) {
+      return this.client.get(url);
+   }
+
+   post<TResponse>(url: string, data?: any) {
+      return this.client.post(url, data);
    }
 }
 
