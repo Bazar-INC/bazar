c69e095634c8d105e2d0f1c71efc657eb002b6c3
Dima <65868322+byCodeXp@users.noreply.github.com>
2022-06-25 17:12:14 +0300

    load user after page refresh

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,29 @@
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
+import { AccountEndpoints } from "./api/endpoints/account";
 import { SignPage } from './components/pages/sign';
+import { accountActions } from "./features/account/reducer";
+import { useAppDispatch, useAppSelector } from "./store/hooks";
 
 const App: FC = () => {
-   return <SignPage />;
+
+   const dispatch = useAppDispatch();
+
+   const profile = useAppSelector(state => state.accountReducer.profile);
+
+   useEffect(() => {
+
+      AccountEndpoints.getProfile().then((response) => {
+         dispatch(accountActions.setProfile(response.data));
+      });
+
+   }, []);
+
+   return (
+      <div className="h-full">
+         <header>User: {profile.name}</header>
+         <SignPage />
+      </div>
+   );
 };
 
 export { App };
