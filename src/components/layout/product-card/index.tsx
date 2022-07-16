a54c8c8c9683988faff8fa2595ef795d01f949bc
Dima <65868322+byCodeXp@users.noreply.github.com>
2022-07-16 22:26:41 +0300

    update product card component

--- src/components/layout/product-card/index.tsx
+++ src/components/layout/product-card/index.tsx
@@ -2,7 +2,7 @@ import { FC } from 'react';
 import { Icons } from '../../icons/icons';
 import { Layout } from '../layout';
 
-import './product-card.css';
+import './index.css';
 
 interface Props {
    picture: string;
@@ -13,21 +13,21 @@ interface Props {
 
 const ProductCard: FC<Props> = ({ picture, categoryName, productName, price }) => {
    return (
-      <div className="w-[400px] py-10 flex flex-col items-center bg-white rounded-lg transition product-card">
-         <img className="h w-72 h-[360px] object-contain" src={picture} />
+      <div className="card-product">
+         <img className="card-image" src={picture} />
          <div className="w-72 mt-6 flex flex-col items-start">
-            <span className="font-bold text-[#61615F] text-[15px] mb-2">{categoryName}</span>
-            <span className="font-medium text-[20px]">{productName}</span>
-            <Layout.Badge additionalClasses="mt-4" color="#00FF74">Від 1099 в міс.</Layout.Badge>
-            <Layout.Badge additionalClasses="mt-4" color="#8F00F9">Хіт продаж</Layout.Badge>
+            <span className="font-[Intro] text-[#61615f] text-[15px] mb-2">{categoryName}</span>
+            <span className="font-semibold text-[17px] h-12 overflow-clip">{productName}</span>
+            <Layout.Badge additionalClasses="mt-4" color="#00ff74">Від 1099 в міс.</Layout.Badge>
+            <Layout.Badge additionalClasses="mt-4" color="#8f00f9">Хіт продаж</Layout.Badge>
             <div className="flex mt-5 items-center">
-               <span className="line-through text-[#70706D] font-bold text-xs">17 999</span>
-               <span className="bg-[#D4FFE7] rounded ml-2 font-bold text-xs text-[#1D1D1B] px-2.5">-1000</span>
+               <span className="line-through text-[#70706d] font-semibold text-[10px]">17 999</span>
+               <span className="bg-[#d4ffe7] rounded ml-2 font-semibold text-[10px] text-[#1d1d1d] px-2.5">-1000</span>
             </div>
-            <span className="text-[20px] font-bold">{price} грн</span>
+            <span className="text-[20px] font-[Intro]">{price} грн</span>
             <div className="w-full flex mt-5">
                <Layout.Button stretch>Купити</Layout.Button>
-               <Icons.Compare className="text-[#8F00F9] mx-7 w-16" />
+               <Icons.Compare className="text-[#8f00f9] ml-7 w-16 cursor-pointer" />
             </div>
          </div>
       </div>
