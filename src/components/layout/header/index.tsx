cafe3e386e44175a1b0c1eb738f08da53fed366e
Dima <65868322+byCodeXp@users.noreply.github.com>
2022-07-19 11:16:09 +0300

    add navigation links and icon for navbar

--- src/components/layout/header/index.tsx
+++ src/components/layout/header/index.tsx
@@ -1,5 +1,6 @@
 import { FC } from 'react';
 import { Link } from 'react-router-dom';
+import { Icons } from '../../icons/icons';
 import { Layout } from '../layout';
 
 interface Props {
@@ -19,7 +20,23 @@ const Header: FC<Props> = ({ openSignModal }) => {
                </div>
             </div>
          </header>
-         <nav className="h-[70px] bg-[#1d1d1b]"></nav>
+         <nav className="h-[70px] bg-[#1d1d1b]">
+            <div className="w-[1660px] mx-auto flex items-center h-full">
+               <div className="w-[430px] h-full bg-[#31353c] flex items-center">
+                  <span className="ml-5 mr-2.5"><Icons.AlignLeft /></span>
+                  <span className="text-[21px] font-semibold text-white leading-[70px]">Каталог товарів</span>
+               </div>
+               <div className="text-white font-semibold text-[20px] space-x-12 ml-8">
+                  <span>Про нас</span>
+                  <span>Довідковий центр</span>
+                  <span>Доставка та оплата</span>
+                  <span>Партнерам</span>
+               </div>
+               <div className="ml-auto">
+                  <span className="text-white font-semibold text-[20px]">Покупцям</span>
+               </div>
+            </div>
+         </nav>
       </>
    );
 };
