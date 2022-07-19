e3664650e4057cb0e051787f33cc6ce370d11e01
Dima <65868322+byCodeXp@users.noreply.github.com>
2022-07-19 11:15:20 +0300

    add AlignLeft icon component

--- src/components/icons/icons.ts
+++ src/components/icons/icons.ts
@@ -1,9 +1,11 @@
+import { AlignLeft } from './align-left';
 import { Cart } from './cart';
 import { Compare } from './compare';
 import { Location } from './location';
 import { Search } from './search';
 
 const Icons = {
+   AlignLeft,
    Cart,
    Compare,
    Location,
