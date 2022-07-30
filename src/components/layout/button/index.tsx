b255fe52ee7c50df28b063ac0722e71be155dcbe
Sidanton6006 <61793320+Sidanton6006@users.noreply.github.com>
2022-07-30 12:33:56 +0300

    add onClick prop for button component

--- src/components/layout/button/index.tsx
+++ src/components/layout/button/index.tsx
@@ -3,14 +3,15 @@ import { FC } from 'react';
 interface Props {
    children: string;
    stretch?: boolean;
+   onClick?(): void;
 }
 
-const Button: FC<Props> = ({ children, stretch: stretch = false }) => {
+const Button: FC<Props> = ({ children, stretch: stretch = false, onClick }) => {
 
    const stretchStyles = stretch === true ? 'w-full' : '';
 
    return (
-      <button className={`bg-[#8F00F9] hover:bg-[#6500B0] text-white font-['Poppins'] py-3 px-7 rounded ${stretchStyles}`}>
+      <button onClick={onClick} className={`text-[25px] bg-[#8F00F9] hover:bg-[#6500B0] text-white py-3 px-7 rounded ${stretchStyles}`}>
          {children}
       </button>
    );
