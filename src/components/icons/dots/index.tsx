4b90038dbc16ac2c5a76f838a4421e02d3d6b0d0
Sidanton6006 <61793320+Sidanton6006@users.noreply.github.com>
2022-07-30 14:20:37 +0300

    add a few icon components

---
+++ src/components/icons/dots/index.tsx
@@ -0,0 +1,13 @@
+import { FC } from 'react';
+
+const Dots: FC = () => {
+   return (
+      <svg width="34" height="8" viewBox="0 0 34 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+         <circle cx="4" cy="4" r="3" stroke="#8F00F9" strokeWidth="2" />
+         <circle cx="17" cy="4" r="3" stroke="#8F00F9" strokeWidth="2" />
+         <circle cx="30" cy="4" r="3" stroke="#8F00F9" strokeWidth="2" />
+      </svg>
+   );
+};
+
+export { Dots };
--- src/components/icons/icons.ts
+++ src/components/icons/icons.ts
@@ -1,14 +1,20 @@
 import { AlignLeft } from './align-left';
 import { Cart } from './cart';
 import { Compare } from './compare';
+import { Controller } from './controller';
+import { Dots } from './dots';
 import { Location } from './location';
+import { Phone } from './phone';
 import { Search } from './search';
 
 const Icons = {
    AlignLeft,
    Cart,
    Compare,
+   Controller,
+   Dots,
    Location,
+   Phone,
    Search
 };
 
