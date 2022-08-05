4e8d64d3ff448b30207b49f17c9abe79d1a08722
Dima <65868322+byCodeXp@users.noreply.github.com>
2022-08-05 19:30:04 +0200

    refactor goods page

--- src/components/pages/goods/index.tsx
+++ src/components/pages/goods/index.tsx
@@ -1,31 +1,30 @@
-import React, { FC, useEffect } from 'react';
+import React, { FC, useEffect } from "react";
 import { ProductsEndpoints } from "../../../api/endpoints/products";
 import { useProperty } from "../../hooks/property";
-import { Layout } from '../../layout/layout';
-import { Typography } from '../../typography';
+import { Layout } from "../../layout/layout";
+import { Typography } from "../../typography";
+import { Filter } from "./components/filter";
 
-interface FilterSectionProps {
-   title: string;
-   values: string[];
-}
+const breadCrumbsItems = [
+   { label: 'Головна', route: '' },
+   { label: 'Каталог', route: '' },
+   { label: 'Літній розпродаж', route: '' },
+];
 
-const FilterSection: FC<FilterSectionProps> = ({ title, values }) => {
-   return (
-      <div>
-         <span className="font-bold text-[20px]">{title}</span>
-         <ol>
-            {values.map((value, index) => (
-               <li className="mt-2.5" key={index}>
-                  <label className="flex items-center">
-                     <input className="w-[30px] h-[30px]" type="checkbox" />
-                     <span className="text-[15px] font-semibold ml-4">{value}</span>
-                  </label>
-               </li>
-            ))}
-         </ol>
-      </div>
-   );
-};
+const mockPics = [
+   "https://itmag.ua/upload/iblock/a2c/piktjhio4tmj4z0kn2sjw3rert35ibiu/195u.jpg",
+   "https://content2.rozetka.com.ua/goods/images/big/249084760.jpg",
+   "https://content2.rozetka.com.ua/goods/images/big/273452464.jpg",
+   "https://img.ktc.ua/img/base/1/0/400130.jpg",
+   "https://content2.rozetka.com.ua/goods/images/big/243680271.jpg",
+   "https://content1.rozetka.com.ua/goods/images/big/225058937.jpg",
+   "https://content2.rozetka.com.ua/goods/images/big/137115749.jpg",
+   "https://content1.rozetka.com.ua/goods/images/big/10732647.jpg",
+   "https://itmag.ua/upload/iblock/a2c/piktjhio4tmj4z0kn2sjw3rert35ibiu/195u.jpg",
+   "https://content2.rozetka.com.ua/goods/images/big/249084760.jpg",
+   "https://content2.rozetka.com.ua/goods/images/big/273452464.jpg",
+   "https://img.ktc.ua/img/base/1/0/400130.jpg"
+];
 
 const GoodsPage: FC = () => {
 
@@ -35,52 +34,25 @@ const GoodsPage: FC = () => {
 
    useEffect(() => {
 
-      ProductsEndpoints.getProducts().then((response) => {
-
-         const data = response.data;
+      ProductsEndpoints.getProducts().then(({ data }) => {
 
          products.set(data.products.map((p) => ({ title: p.name, price: p.price })));
-         totalPages.set(data.totalPages);
 
-         const ff = data.filters.map((f) => {
-            return {
+         filters.set(
+            data.filters.map((f) => ({
                title: f.name,
                values: f.options.map((v) => v.value)
-            };
-         });
+            }))
+         );
 
-         filters.set(ff);
+         totalPages.set(data.totalPages);
       });
 
    }, []);
 
-   const mockPics = [
-      "https://itmag.ua/upload/iblock/a2c/piktjhio4tmj4z0kn2sjw3rert35ibiu/195u.jpg",
-      "https://content2.rozetka.com.ua/goods/images/big/249084760.jpg",
-      "https://content2.rozetka.com.ua/goods/images/big/273452464.jpg",
-      "https://img.ktc.ua/img/base/1/0/400130.jpg",
-      "https://content2.rozetka.com.ua/goods/images/big/243680271.jpg",
-      "https://content1.rozetka.com.ua/goods/images/big/225058937.jpg",
-      "https://content2.rozetka.com.ua/goods/images/big/137115749.jpg",
-      "https://content1.rozetka.com.ua/goods/images/big/10732647.jpg",
-      "https://itmag.ua/upload/iblock/a2c/piktjhio4tmj4z0kn2sjw3rert35ibiu/195u.jpg",
-      "https://content2.rozetka.com.ua/goods/images/big/249084760.jpg",
-      "https://content2.rozetka.com.ua/goods/images/big/273452464.jpg",
-      "https://img.ktc.ua/img/base/1/0/400130.jpg",
-      "https://content2.rozetka.com.ua/goods/images/big/243680271.jpg",
-      "https://content1.rozetka.com.ua/goods/images/big/225058937.jpg",
-      "https://content2.rozetka.com.ua/goods/images/big/137115749.jpg",
-      "https://content1.rozetka.com.ua/goods/images/big/10732647.jpg",
-      "https://itmag.ua/upload/iblock/a2c/piktjhio4tmj4z0kn2sjw3rert35ibiu/195u.jpg"
-   ];
-
    return (
       <div className="max-w-[1660px] mx-auto px-4 mt-8">
-         <Layout.BreadCrumbs items={[
-            { label: 'Головна', route: '' },
-            { label: 'Каталог', route: '' },
-            { label: 'Літній розпродаж', route: '' },
-         ]} />
+         <Layout.BreadCrumbs items={breadCrumbsItems} />
          <Typography.Heading className="block mt-5">Літній розпродаж</Typography.Heading>
          <div className="flex gap-x-[100px] mt-14">
             <div className="w-[400px]">
@@ -88,7 +60,7 @@ const GoodsPage: FC = () => {
                {filters.get.map((filter, index) => (
                   <React.Fragment key={index}>
                      <hr className="my-8" />
-                     <FilterSection title={filter.title} values={filter.values} />
+                     <Filter title={filter.title} values={filter.values} />
                   </React.Fragment>
                ))}
             </div>
