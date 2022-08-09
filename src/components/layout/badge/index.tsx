3d33627f6c53f684fe6a1ad0219fb5d7ada886e0
Dima <65868322+byCodeXp@users.noreply.github.com>
2022-08-09 07:16:57 +0200

    update badge component

--- src/components/layout/badge/index.tsx
+++ src/components/layout/badge/index.tsx
@@ -10,9 +10,9 @@ const Badge: FC<Props> = ({ children, color, additionalClasses }) => {
    return (
       <div
          style={{ borderColor: color }}
-         className={`inline-block font-semibold text-[15px] py-1 px-2.5 border-2 rounded-lg ${additionalClasses ?? ''}`}
+         className={`inline-block rounded-lg px-1 py-0.5 border-2 ${additionalClasses ?? ''}`}
       >
-         {children}
+         <span className="text-sm font-[Gotham] font-bold">{children}</span>
       </div>
    );
 };
