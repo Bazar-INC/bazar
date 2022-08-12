237a0efa2a979cdf874a8cbe165872dea3e905d3
Dima <65868322+byCodeXp@users.noreply.github.com>
2022-08-12 12:39:02 +0200

    Added new sections to home page

--- src/components/pages/home/index.tsx
+++ src/components/pages/home/index.tsx
@@ -1,5 +1,6 @@
 import { FC } from "react";
 import { Layout } from "../../layout/layout";
+import { Typography } from "../../typography";
 
 const productList1 = [
    {
@@ -59,10 +60,14 @@ const HomePage: FC = () => {
    return (
       <div className="py-8">
          <Layout.Container>
-            <div className="h-[595px] w-full ml-auto rounded-lg bg-black"></div>
+            <div className="flex">
+               <div className="w-[430px] h-2"></div>
+               <div className="ml-8 flex-1 h-[608px] w-full rounded-lg bg-black"></div>
+            </div>
          </Layout.Container>
          <img className="mt-20 mb-10" src="/banner.png" />
          <Layout.Container>
+
             <div className="flex items-center">
                <img src="/fire.png" />
                <span className="font-[Intro] text-[40px] mt-4">Хіти продаж</span>
@@ -72,6 +77,24 @@ const HomePage: FC = () => {
                   <Layout.ProductCard key={index} {...product} />
                ))}
             </div>
+
+            <div className="mt-32 flex gap-x-24">
+               <div className="flex bg-white px-12 py-16 rounded-xl h-64">
+                  <div className="flex flex-col items-start">
+                     <Layout.Badge color="#8f00f9" additionalClasses="w-[145px]">Огляд</Layout.Badge>
+                     <Typography.Heading className="mt-5 text-[30px]">Спорт обладнання</Typography.Heading>
+                  </div>
+                  <img className="h-80 -mt-32" src="/girya.png" />
+               </div>
+               <div className="flex bg-white px-12 py-16 rounded-xl h-64">
+                  <div className="flex flex-col">
+                     <Layout.Badge color="#00ff74" additionalClasses="w-[145px]">Огляд</Layout.Badge>
+                     <Typography.Heading className="mt-5 text-[30px]">Смарт годинник</Typography.Heading>
+                  </div>
+                  <img className="h-80 -mt-32 w-full" src="/clock.png" />
+               </div>
+            </div>
+
             <div className="flex items-center mt-20">
                <img src="/sound.png" />
                <span className="font-[Intro] text-[40px] mt-4 ml-2">Новинки</span>
@@ -80,6 +103,23 @@ const HomePage: FC = () => {
                {productList2.map((product, index) => (
                   <Layout.ProductCard key={index} {...product} />
                ))}
+            </div>
+
+            <div className="my-40 flex gap-x-24">
+               <div className="flex bg-white px-12 py-16 rounded-xl h-64">
+                  <div className="flex flex-col items-start">
+                     <Layout.Badge color="#00ff74" additionalClasses="w-[145px]">Розтрочка</Layout.Badge>
+                     <Typography.Heading className="mt-5 text-[30px]">Плати частинами</Typography.Heading>
+                  </div>
+                  <img className="h-96 -mt-48" src="/money.png" />
+               </div>
+               <div className="flex bg-white px-12 py-16 rounded-xl h-64">
+                  <div className="flex flex-col">
+                     <Layout.Badge color="#8f00f9" additionalClasses="w-[145px]">Категорії</Layout.Badge>
+                     <Typography.Heading className="mt-5 text-[30px]">Фільтруй базар</Typography.Heading>
+                  </div>
+                  <img className="h-96 -mt-48 w-full" src="/txt.png" />
+               </div>
             </div>
          </Layout.Container>
       </div>
