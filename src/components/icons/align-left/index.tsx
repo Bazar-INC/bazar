22dab7765e61bdb0a7b079455ad5ef3db5e804de
Dima <65868322+byCodeXp@users.noreply.github.com>
2022-08-18 20:04:47 +0200

    added className prop to AlignLeft icon component

--- src/components/icons/align-left/index.tsx
+++ src/components/icons/align-left/index.tsx
@@ -1,8 +1,13 @@
 import { FC } from 'react';
+import { classes } from "../../../functions";
 
-const AlignLeft: FC = () => {
+interface Props {
+   className?: string;
+}
+
+const AlignLeft: FC<Props> = ({ className }) => {
    return (
-      <svg width="35" height="44" viewBox="0 0 35 44" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <svg className={classes("w-9 h-9", className)} viewBox="0 0 35 44" fill="none" xmlns="http://www.w3.org/2000/svg">
          <path d="M5.46875 6.74463V36.4209" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
          <path d="M24.0625 9.4425H10.9375C10.3334 9.4425 9.84375 10.0464 9.84375 10.7914V17.536C9.84375 18.281 10.3334 18.885 10.9375 18.885H24.0625C24.6666 18.885 25.1562 18.281 25.1562 17.536V10.7914C25.1562 10.0464 24.6666 9.4425 24.0625 9.4425Z" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
          <path d="M29.5312 24.2806H10.9375C10.3334 24.2806 9.84375 24.8846 9.84375 25.6296V32.3742C9.84375 33.1192 10.3334 33.7231 10.9375 33.7231H29.5312C30.1353 33.7231 30.625 33.1192 30.625 32.3742V25.6296C30.625 24.8846 30.1353 24.2806 29.5312 24.2806Z" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
