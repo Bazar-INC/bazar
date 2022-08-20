e0fc9899a918b003f112919f3446caaf74081f78
Dima <65868322+byCodeXp@users.noreply.github.com>
2022-08-20 15:09:28 +0200

    added classname prop for location icon component

--- src/components/icons/location/index.tsx
+++ src/components/icons/location/index.tsx
@@ -1,8 +1,12 @@
-import { FC } from 'react';
+import { FC } from "react";
 
-const Location: FC = () => {
+interface Props {
+   className?: string;
+}
+
+const Location: FC<Props> = ({ className }) => {
    return (
-      <svg width="22" height="30" viewBox="0 0 22 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <svg className={className} viewBox="0 0 22 30" fill="none" xmlns="http://www.w3.org/2000/svg">
          <path d="M11 14C13.2348 14 15.0465 12.2091 15.0465 10C15.0465 7.79086 13.2348 6 11 6C8.76526 6 6.95361 7.79086 6.95361 10C6.95361 12.2091 8.76526 14 11 14Z" stroke="#464D5E" strokeOpacity="0.8" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
          <path d="M19.0929 14C16.058 21 11 29 11 29C11 29 5.94196 21 2.90714 14C-0.127685 7 4.93035 1 11 1C17.0696 1 22.1277 7 19.0929 14Z" stroke="#464D5E" strokeOpacity="0.8" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
       </svg>
