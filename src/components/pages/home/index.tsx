b3a1b914754b5cee41914f0665c3822abf3b8efe
Dima <65868322+byCodeXp@users.noreply.github.com>
2022-08-27 15:45:29 +0200

    move CustomCard component to own file

--- src/components/pages/home/index.tsx
+++ src/components/pages/home/index.tsx
@@ -4,28 +4,7 @@ import { ProductModel } from "../../../api/models/product";
 import { useProperty } from "../../hooks/property";
 import { Layout } from "../../layout/layout";
 import { Typography } from "../../typography";
-
-interface CustomCardProps {
-   title: string;
-   badge: string;
-   picture: string;
-}
-
-const CustomCard: FC<CustomCardProps> = ({ title, badge, picture }) => {
-   return (
-      <div className="flex justify-between lg:justify-start bg-white px-8 2xl:px-12 py-8 2xl:py-16 rounded-xl h-40 2xl:h-64 w-full sm:w-auto">
-         <div className="flex flex-col">
-            <Layout.Badge color="#00ff74" additionalClasses="w-[145px]">
-               {badge}
-            </Layout.Badge>
-            <Typography.Heading className="mt-5 !text-[20px] !2xl:text-[30px]">
-               {title}
-            </Typography.Heading>
-         </div>
-         <img className="ml-auto sm:ml-0 h-32 sm:h-56 2xl:h-96 sm:-mt-24 2xl:-mt-48" src={picture} />
-      </div>
-   );
-};
+import { CustomCard } from "./components/custom-card";
 
 const HomePage: FC = () => {
 
