27c811987792d6b0a8d499494508eaeed78dec1a
Dima <65868322+byCodeXp@users.noreply.github.com>
2022-09-03 20:00:27 +0200

    added endpoints for categories api service

--- src/api/endpoints/categories.ts
+++ src/api/endpoints/categories.ts
@@ -3,10 +3,24 @@ import { CategoryModel } from "../models/category";
 
 const client = new Client("categories");
 
+interface CategoriesResponse {
+   categories: Array<CategoryModel>;
+}
+
+const getAllCategories = () => {
+   return client.get<CategoriesResponse>("");
+};
+
 const getCategory = (code: string) => {
    return client.get<CategoryModel>(`/${code}`);
 };
 
+const deleteCategory = (id: string) => {
+   return client.delete(`/delete/${id}`);
+};
+
 export const CategoriesEndpoints = {
-   getCategory
+   getCategory,
+   getAllCategories,
+   deleteCategory,
 };
