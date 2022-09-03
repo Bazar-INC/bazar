57e2363e3c5019dfe3037b97df90e5fe811e44c8
Dima <65868322+byCodeXp@users.noreply.github.com>
2022-09-03 20:19:03 +0200

    added endpoint for creating new category to api service

--- src/api/endpoints/categories.ts
+++ src/api/endpoints/categories.ts
@@ -19,8 +19,23 @@ const deleteCategory = (id: string) => {
    return client.delete(`/delete/${id}`);
 };
 
+interface NewCategory {
+   name: string;
+   code: string;
+   imageName?: string;
+   image?: string;
+   iconName?: string;
+   icon?: string;
+   parentId?: string;
+}
+
+const addCategory = (category: NewCategory) => {
+   return client.post("/add", category);
+};
+
 export const CategoriesEndpoints = {
    getCategory,
    getAllCategories,
    deleteCategory,
+   addCategory,
 };
