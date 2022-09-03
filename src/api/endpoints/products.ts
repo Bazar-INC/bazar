ae948bd092df3ba0beafec4df78aa3f2fed92e0d
Dima <65868322+byCodeXp@users.noreply.github.com>
2022-09-03 20:36:26 +0200

    implemented deleting product on click

--- src/api/endpoints/products.ts
+++ src/api/endpoints/products.ts
@@ -30,8 +30,13 @@ const getProductById = (id: string) => {
    return client.get<ProductModel>(`/${id}`);
 };
 
+const deleteProduct = (id: string) => {
+   return client.delete(`/delete/${id}`);
+};
+
 export const ProductsEndpoints = {
    getProducts,
    getProductById,
    getTopProducts,
+   deleteProduct,
 };
